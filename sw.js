f3bd0cb14af731f992d9a0c3e422dc5c5455fe11
jeanphilippekoll-cpu <jeanphilippekoll@icloud.com>
2026-03-30 12:12:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,46 +1,44 @@
-const CACHE_NAME = "rapaces-app-v18";
+const CACHE_NAME = "rapaces-v1";
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon.png"
+const FILES_TO_CACHE = [
+  "/fiche-rapaces/",
+  "/fiche-rapaces/index.html",
+  "/fiche-rapaces/style.css",
+  "/fiche-rapaces/app.js",
+  "/fiche-rapaces/icon.png",
+  "/fiche-rapaces/apple-touch-icon.png",
+  "/fiche-rapaces/manifest.json"
 ];
 
+// INSTALL
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
-  self.skipWaiting();
 });
 
+// ACTIVATE
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
+    caches.keys().then((keys) => {
+      return Promise.all(
         keys.map((key) => {
           if (key !== CACHE_NAME) {
             return caches.delete(key);
           }
         })
-      )
-    )
+      );
+    })
   );
-  self.clients.claim();
 });
 
+// FETCH
 self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
   event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const responseClone = response.clone();
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseClone);
-        });
-        return response;
-      })
-      .catch(() => caches.match(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
